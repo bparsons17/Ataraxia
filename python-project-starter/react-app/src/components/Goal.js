533b2fb8254eb705112d5bf85562460c60a5a8b7
brandon parsons <bparsons17@liberty.edu>
2021-04-13 20:06:22 -0400

    updated edit and delete buttons on goal page

--- python-project-starter/react-app/src/components/Goal.js
+++ python-project-starter/react-app/src/components/Goal.js
@@ -1,77 +1,86 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { seeGoal } from '../store/goals'
-import {Button} from 'antd'
+import { Button } from 'antd'
 import 'antd/dist/antd.css';
 import './style/goal.css'
 import Info from '../components/Info'
-import {Route} from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
-const Goal = ({user, goal}) => {
-    const dispatch = useDispatch();
-    const sessionUser = useSelector((state) => state.session.user);
-    const sessionGoals = useSelector((state) => (state.goal.goal))
-    console.log(sessionGoals)
-    
-
-    useEffect(()=> {
-        dispatch(seeGoal())
-    }, [])
+const Goal = ({ user, goal }) => {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const sessionGoals = useSelector((state) => (state.goal.goal))
+  console.log(sessionGoals)
 
 
-    console.log(sessionGoals)
+  useEffect(() => {
+    dispatch(seeGoal())
+  }, [])
 
-    return (
-    <div className='yug'>{sessionGoals && 
-        sessionGoals.map((goal)=> (
-          <div key={goal.id} className='wrapper2 border-l-2 border-r-2 border-gray-600'>
-          <div class="flex flex-shrink-0 p-4 pb-0 justify-center"> 
-                  <div class="flex  flex-column ">
-                      <div class="ml-3 flex flex-column w-full">
-                         
-                          <div class="text-base font-bold text-gray-400 flex-shrink float-right">
+
+  console.log(sessionGoals)
+
+  return (
+    <div className='yug'>{sessionGoals &&
+      sessionGoals.map((goal) => (
+        <div key={goal.id} className='wrapper2 border-l-2 border-r-2 border-gray-600'>
+          <div class="flex flex-shrink-0 p-4 pb-0 justify-center">
+            <div class="flex  flex-column ">
+              <div class="ml-3 flex flex-column w-full">
+
+                <div class="text-base font-bold text-gray-400 flex-shrink float-right">
                   {goal.title}
-                          </div>
-              
-                          
-                      </div>
-                      
-                  </div>
-              
-          </div>
-          <div class="pl-10 pr-10">
-          <div class="text-base leading-6 font-bold text-gray-400">
-                              
-              <span class="text-sm leading-5 font-bold text-gray-400 ">
-                  Goal Description: {goal.description}
-              </span>
-          </div>
-
-              <div class="flex">
-                  <div class="w-full">
-
-                      <div className="flex items-center leading-6 text-xl text-gray-400">
-                          <span className="flex-1 text-sm leading-5 font-bold text-grey-400  "> steps to complete goal: {goal.steps}
-                          
-                          </span>
-                    
-                          <Info goal={goal}/>
-                              
-                      </div>
-                  </div>
+                </div>
 
 
               </div>
-              <span className='flex-1 text-sm leading-5 font-bold text-gray-400' >
-                                Complete by: {goal.deadline}
-                          </span>
+
+            </div>
+
+          </div>
+          <div class="pl-10 pr-10">
+            <div class="text-base leading-6 font-bold text-gray-400">
+
+              <span class="text-sm leading-5 font-bold text-gray-400 ">
+                Goal Description: {goal.description}
+              </span>
+            </div>
+
+            <div class="flex">
+              <div class="w-full">
+
+                <div className="flex items-center leading-6 text-xl text-gray-400">
+                  <span className="flex-1 text-sm leading-5 font-bold text-grey-400  "> steps to complete goal: {goal.steps}
+
+                  </span>
+
+
+
+                </div>
+              </div>
+
+            </div>
+            <div className='spacing flex-1 flex flex-col justify-end text-sm leading-5 font-bold text-gray-400 w-50' >
+              Complete by: {goal.deadline}
+
+              <div className='edit'>
+                <Info goal={goal} />
+              </div>
+                 
+            
+               
+          
+            </div>
+
+
 
           </div>
 
 
           <hr class="border-b-2 border-gray-600"></hr>
 
-      </div>
+        </div>
         //   <div className='col-1'>
         //     <div key={goal.id} class="idk p-5">  
         //     <div class="card max-w-sm rounded overflow-hidden shadow-lg">
@@ -90,19 +99,19 @@ const Goal = ({user, goal}) => {
         //       </div>
         //       <div class="px-3 pt-2 pb-2">
         //         {/* <button class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Mark as Completed
-                    
+
         //         </button> */}
         //         <div className='btn-holder rounded-full'>
         //         <Info goal={goal}></Info>
         //         </div>
-                
+
         //       </div>
         //     </div>
         //   </div>
         //    </div>
-        )) }
-        </div>
-    )
+      ))}
+    </div>
+  )
 }
 
 export default Goal;
--- python-project-starter/react-app/src/components/Info.js
+++ python-project-starter/react-app/src/components/Info.js
@@ -1,37 +1,37 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import {  Button, Modal } from "antd";
+import { Button, Modal } from "antd";
 import 'antd/dist/antd.css';
 import { deleteGoal, seeGoal } from "../store/goals";
 
 
 const Info = ({ goal }) => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [editVisibility, setEditVisibility] = useState(false);
-    const [title, setTitle] = useState(goal.title)
-    const [description, setDescription] = useState(goal.description)
-    const [deadline, setDeadline] = useState(goal.deadline)
-    const [steps, setSteps] = useState(goal.steps)
-    const dispatch = useDispatch();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [editVisibility, setEditVisibility] = useState(false);
+  const [title, setTitle] = useState(goal.title)
+  const [description, setDescription] = useState(goal.description)
+  const [deadline, setDeadline] = useState(goal.deadline)
+  const [steps, setSteps] = useState(goal.steps)
+  const dispatch = useDispatch();
 
-    const goalId = goal.id;
-    console.log(goalId)
+  const goalId = goal.id;
+  console.log(goalId)
 
-    
 
-    const showModal = () => {
-        setIsModalVisible(true);
-      };
-    
-      const handleOk = () => {
-        setIsModalVisible(false);
-      };
-    
-      const handleCancel = () => {
-        setIsModalVisible(false);
-      };
 
-    
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+
   const onGoalUpdate = async (e) => {
     e.preventDefault();
     goal = { title, steps, description, deadline };
@@ -49,81 +49,86 @@ const Info = ({ goal }) => {
 
   };
 
-    async function deleteOneGoal() {
-        await dispatch(deleteGoal(goal.id))
-    }
+  async function deleteOneGoal() {
+    await dispatch(deleteGoal(goal.id))
+  }
 
-  
-    return (
-        goal !== undefined && (
-            <div>
-                <button className="goal_submit_button " type="primary" onClick={showModal}>Update Goal</button>
 
-                <Modal
-                  visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={[
-                    ,
-                    <button key="submit" type="submit" onClick={handleOk} onSubmit={onGoalUpdate}>
+  return (
+    goal !== undefined && (
+      <div>
+
+
+        <Modal
+          visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={[
+            ,
+            <button key="submit" type="submit" onClick={handleOk} onSubmit={onGoalUpdate}>
               Close </button>,
-  
-                  ]}
-                >
-                    <form onSubmit={onGoalUpdate}>
-                        <div className="column_border">
-                            <label for="title" className="flex text-sm font-medium text-gray-300">Goal</label>
-                            <input
-                                className="input shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
-                                name="goal_name"
-                                placeholder={goal.title}
-                                type="text"
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
-                            />
-                        </div>
-                        <div className="column_border">
-                            <label for="description" className="flex text-sm font-medium text-gray-300">Description</label>
-                            <textarea
-                                className="input shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
-                                name="goal_description"
-                                placeholder={goal.description}
-                                type="text"
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
-                            />
-                        </div>
-                        <div className="column_border">
-                            <label for="steps" className="flex text-sm font-medium text-gray-300">Steps</label>
-                            <textarea
-                                className="input shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
-                                name="goal_steps"
-                                placeholder={goal.steps}
-                                type="text"
-                                value={steps}
-                                onChange={(e) => setSteps(e.target.value)}
-                            />
-                        </div>
-                        <div className="column_border">
-                            <label for="deadline" className="flex text-sm font-medium text-gray-300">Accomplish by</label>
-                            <input
-                                className="input shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
-                                name="goal_deadline"
-                                placeholder={goal.deadline}
-                                type="date"
-                                value={deadline}
-                                onChange={(e) => setDeadline(e.target.value)}
-                            />
-                        </div>
-                        <div  className='update' >
-                        <button className='modal_btns' type='primary' style={{float:'right'}, {padding: '3px'}, {margin:'3px'} } onClick={deleteOneGoal}>Delete Goal</button>
-                        <button className='modal_btns' type='primary' type='submit'>Update Goal</button>
-                            
-                        </div>
 
-                    </form>
-                </Modal>
+          ]}
+        >
+          <form onSubmit={onGoalUpdate}>
+            <div className="column_border">
+              <label for="title" className="flex text-sm font-medium text-gray-300">Goal</label>
+              <input
+                className="input shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
+                name="goal_name"
+                placeholder={goal.title}
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="column_border">
+              <label for="description" className="flex text-sm font-medium text-gray-300">Description</label>
+              <textarea
+                className="input shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
+                name="goal_description"
+                placeholder={goal.description}
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div className="column_border">
+              <label for="steps" className="flex text-sm font-medium text-gray-300">Steps</label>
+              <textarea
+                className="input shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
+                name="goal_steps"
+                placeholder={goal.steps}
+                type="text"
+                value={steps}
+                onChange={(e) => setSteps(e.target.value)}
+              />
+            </div>
+            <div className="column_border">
+              <label for="deadline" className="flex text-sm font-medium text-gray-300">Accomplish by</label>
+              <input
+                className="input shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
+                name="goal_deadline"
+                placeholder={goal.deadline}
+                type="date"
+                value={deadline}
+                onChange={(e) => setDeadline(e.target.value)}
+              />
+            </div>
+            <div className='update' >
+              <button className='modal_btns' type='primary' type='submit'>Update Goal</button>
 
             </div>
-        )
+
+          </form>
+        </Modal>
+        <div className='x-button edit ' >
+          <div onClick={showModal} className='border-2 p-1 rounded-sm m-2 border-gray-400 p-1'>Edit</div>
+          <button onClick={deleteOneGoal}>
+            <svg className='hover:bg-indigo-800 hover:text-indigo-300' fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z" /></svg>
+          </button>
+        </div>
+
+      </div>
     )
+  )
 }
 
 export default Info;
